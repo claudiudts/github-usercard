b26c17be15159f73aff6caeb40be11d1597d0f1a
Claudiuts <sarb.claudiu391@gmail.com>
2020-12-03 21:12:10 -0600

    done

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -3,17 +3,21 @@
     (replacing the placeholder with your Github name):
     https://api.github.com/users/<your name>
 */
-
 import axios from 'axios';
-  axios
-  .get('https://api.github.com/users/claudiudts')
-  .then((futureData)=>{
-      const gitInfo = futureData.data;
-      cards.appendChild(cardMaker(gitInfo));
-  })
-  .catch((err)=>{
-    // debugger;
-  })
+
+const cardContainer = document.querySelector('.cards');
+
+axios
+.get(`https://api.github.com/users/claudiudts`)
+.then(res => {
+  const user = res.data
+  const userCard = cardMaker(res.data)
+  cardContainer.appendChild(userCard);
+})
+.catch(err => {
+  console.log(err)
+})
+
 
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
@@ -41,18 +45,68 @@ import axios from 'axios';
 
 const followersArray = ['tetondan','dustinmyers','justsml','luishrd','bigknell'];
 
-followersArray.forEach((each)=>{
-  axios
-  .get('https://api.github.com/users/' + each)
-  .then((futureData)=>{
-    const gitInfo = futureData.data;
-    cards.appendChild(cardMaker(gitInfo));
-  })
-  .catch((err)=>{
-    // debugger;
-  })
-});
+followersArray.forEach(follower => {
+  axios.get(`https://api.github.com/users/${follower}`)
+  .then((res => {
+    const user = res.data;
+    const userCard = cardMaker(user);
+  }))
+})
 
+
+
+
+function cardMaker(obj) {
+  const userCard = document.createElement('div');
+  const userImage = document.createElement('img');
+  const cardInfo = document.createElement('div');
+  const userName = document.createElement('h3');
+  const userUsername = document.createElement('p');
+  const userLocation = document.createElement('p');
+  const userProfile = document.createElement('p');
+  const userLink = document.createElement('a');
+  const userFollowers = document.createElement('p');
+  const userFollowing = document.createElement('p');
+  const userBio = document.createElement('p');
+  const textContainer = document.createElement('div');
+
+
+  userCard.appendChild(userImage);
+  userCard.appendChild(cardInfo);
+  cardInfo.appendChild(userName);
+  cardInfo.appendChild(userUsername);
+  cardInfo.appendChild(userLocation);
+  cardInfo.appendChild(userProfile);
+  cardInfo.appendChild(userLink);
+  cardInfo.appendChild(userFollowers);
+  cardInfo.appendChild(userFollowing);
+  userProfile.appendChild(textContainer);
+  userProfile.appendChild(userLink);
+
+
+  userName.textContent = obj.name;
+  userName.textContent = obj.username;
+  userLocation.textContent = `Location: ${obj.location}`;
+  userFollowers.textContent = `Followers: ${obj.followers}`;
+  userFollowing.textContent = `Following: ${obj.following}`;
+  userBio.textContent = `Bio: ${obj.bio}`;
+  textContainer.textContent = 'Profile: ';
+  userLink.textContent = obj.html_url;
+
+
+
+
+  userCard.classList.add('card');
+  userImage.src = obj.avatar_url;
+  cardInfo.classList.add('card-info');
+  userName.classList.add('name');
+  userUsername.classList.add('username');
+  userLink.href = obj.html_url;
+
+
+  return userCard;
+
+}
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
     Using DOM methods and properties, create and return the following markup:
